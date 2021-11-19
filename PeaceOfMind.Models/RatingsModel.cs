b884f850b341658c4ceabfe174d17d5e9f029593
Jacob <jacobeljones56@gmail.com>
2021-11-18 20:13:23 -0500

    RatingsModel added

---
+++ PeaceOfMind.Models/RatingsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeaceOfMind.Models
+{
+    public class RatingsModel
+    {
+       [Required]
+       public int Professionalism { get; set; }
+       [Required]
+       public int Communication { get; set; }
+       [Required]
+       public int Effectiveness { get; set; }
+       [Required]
+       public int Avaliability { get; set; }
+       [Required]
+       public decimal AverageRating { get; set; }
+    }
+}
