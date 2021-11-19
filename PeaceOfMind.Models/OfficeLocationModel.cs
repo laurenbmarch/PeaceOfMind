8b12ecd75a60db48e4c8a12f8ee7e18ee0914c53
Austin Bridgewater <abridgewater34@gmail.com>
2021-11-18 20:08:37 -0500

    Added OfficeLocationModel

---
+++ PeaceOfMind.Models/OfficeLocationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeaceOfMind.Models
+{
+    public class OfficeLocationModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int AddressNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set;  }
+    }
+}
--- PeaceOfMind.Services/TherapistService.cs
+++ PeaceOfMind.Services/TherapistService.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 namespace PeaceOfMind.Services
 {
     class TherapistService
-    {
-        public bool CreateTherapist()
+    {        
     }
 }
