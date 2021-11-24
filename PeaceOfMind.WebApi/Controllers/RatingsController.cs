63b1195e9630181cedf57de9b086c2d5a7dcb05a
Jacob <jacobeljones56@gmail.com>
2021-11-23 20:57:04 -0500

    Rating service and controller done some of the service work was done on my local dev branch before making my own branch

---
+++ PeaceOfMind.WebApi/Controllers/RatingsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.Identity;
+using PeaceOfMind.Models;
+using PeaceOfMind.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace PeaceOfMind.WebApi.Controllers
+{
+    [Authorize]
+    public class RatingsController : ApiController
+    {
+        private RatingsService CreateRatingService()
+        {
+            var Id = Guid.Parse(User.Identity.GetUserId());
+            var ingredientService = new RatingsService(Id);
+            return ingredientService;
+        }
+
+        public IHttpActionResult Post([FromBody] RatingsModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateRatingService();
+            if (!service.CreateRating(model))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Get()
+        {
+            RatingsService service = CreateRatingService();
+            var ratings = service.GetRatings();
+            return Ok(ratings);
+        }
+
+        public IHttpActionResult GetById([FromUri] int id)
+        {
+            RatingsService service = CreateRatingService();
+            var ratingsById = service.GetRatingsById(id);
+            return Ok(ratingsById);
+        }
+
+        public IHttpActionResult Put([FromUri] int id, [FromBody] RatingsModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateRatingService();
+            if (!service.UpdateRatings(id, model))
+                return InternalServerError();
+            return Ok();
+
+        }
+
+    }
+}
